355619d0e22795e720bbf352487c45b5d75cb8d1
Muniz2811 <pedrohenben2014@gmail.com>
2023-11-30 18:49:04 -0300

    Adicionando tela de login

--- front/artplace/src/App.js
+++ front/artplace/src/App.js
@@ -1,12 +1,23 @@
+import React, {useState} from "react";
+import Login from "./components/loginCadastro/Login";
+import Cadastro from "./components/loginCadastro/Cadastro";
 
 
 function App() {
+  const  [ currentForm, setCurrentForm] = useState('register');
+
+  const toggleForm = (formName) =>{
+    setCurrentForm(formName);
+  }
   return (
-    <div className="App">
-      <button onclick>
-        clique aqui
-      </button>
+    
+    <div className="App"> 
+      {
+        
+        currentForm === "register" ? <Cadastro onFormSwitch = {toggleForm}/>  : <Login onFormSwitch = {toggleForm}/>
+      }
     </div>
+    
   );
 }
 
---
+++ front/artplace/src/components/loginCadastro/Login.js
@@ -0,0 +1,42 @@
+import  React, { useState } from 'react';
+import "./Login.css";
+import pintor_image from '../assets/2 1.png'
+
+
+export const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [password, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
+    return(
+        <div className='page'>
+            <img class="image" src={pintor_image} alt="Imagem na bola"/>
+            <div className='Login-Form-Container'>
+                <form className='Login-form'onSubmit={handleSubmit}>
+                        <div className='inputlogin'>
+                            <label htmlFor='email'></label>
+                            <input value = {email} onChange={(e) => setEmail(e.target.value)} type = 'email' placeholder = 'email'/>
+                        </div>
+                        
+                        <div className='inputlogin'>
+                            <label htmlFor='senha'></label>
+                            <input value = {password} onChange={(e) => setPass(e.target.value)} type = 'password' placeholder='senha' />
+                        </div>
+                        
+                        <button className='botao-cadastro'onClick={( ) => props.onFormSwitch('register')}>Ainda não possui um cadastro?</button>
+
+                        <button className = 'botao-submit-login' type='submit'> Login </button>
+                        
+                </form>
+                
+            </div>
+        </div>
+        
+    );
+}
+
+export default Login;
