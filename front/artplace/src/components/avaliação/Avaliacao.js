28fecf348ccc107872edaace09986eeae334a328
pkbceira03 <cabeceira2003@gmail.com>
2023-12-01 10:59:02 -0300

    colocando a base para conectar com o bank

--- front/artplace/src/components/avaliação/Avaliacao.js
+++ front/artplace/src/components/avaliação/Avaliacao.js
@@ -2,17 +2,64 @@ import "./Avaliacao.css";
 import minhaImagem from '../../images/star.png'; 
 
 
-function Avaliacao() {
+function Avaliacao(/*id do produto*/) {
+  /*
+  const [avaliacoes, setAvaliacoes] = useState([]);
+  const [novaAvaliacao, setNovaAvaliacao] = useState('');
+
+  useEffect(() => {
+    const buscarAvaliacoesDoProduto = async () => {
+      try {
+        const response = await fetch(`url da api${productId}`);
+        if (response.ok) {
+          const data = await response.json();
+          setAvaliacoes(data.avaliacoes);
+        } else {
+          throw new Error('Erro ao buscar as avaliações do produto');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    buscarAvaliacoesDoProduto();
+  }, [productId]);
+
+   const handleSalvarAvaliacao = async () => {
+    try {
+      const response = await fetch(`https://sua-api.com/avaliacoes/${productId}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ texto: novaAvaliacao }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setAvaliacoes([...avaliacoes, data.novaAvaliacao]);
+        setNovaAvaliacao('');
+      } else {
+        throw new Error('Erro ao salvar a avaliação');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  */
     return (
       <div className="avaliacao">
         <div  className="centerAvaliacao">
           <div className="avaliacaoDisponivel">
               <h2>Avaliações</h2>
-              <div className="avaliacaoIndividual">
-                <p>muiot bom</p>
-                <img src={minhaImagem} alt="Minha Imagem" />
-                <p>5</p>
-              </div>
+              {/*{avaliacoes.map((avaliacao, index) => (*/}
+                <div className="avaliacaoIndividual" /*key={index}*/>
+                  <p>{/*avaliacao.texto*/}</p>
+                  <img src={minhaImagem} alt="Minha Imagem" />
+                  <p>{/*avaliacao.nota*/}</p>
+                </div>
+               {/*))}*/}
               <div className="clear"></div>
           </div>
 
@@ -23,9 +70,12 @@ function Avaliacao() {
               type="text"
               name="avaliação"
               placeholder="avaliação"
+              /*value={novaAvaliacao}
+              onChange={(e) => setNovaAvaliacao(e.target.value)}*/
+              
             />
 
-            <button>salvar</button>
+            <button /*onClick={handleSalvarAvaliacao}*/>salvar</button>
 
           </div>
         </div>
